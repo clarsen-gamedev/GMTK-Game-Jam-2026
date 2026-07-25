7355a9052c560b144414264b7ea7e75798b7c8ec
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2026-07-24 20:35:27 -0400

    Changes

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -36,7 +36,7 @@ public class EnemySpawner : MonoBehaviour
         if (spawnPoints.Length == 0) return;
 
         Transform chosenSpawn = spawnPoints[Random.Range(0, spawnPoints.Length)];
-        GameObject prefabToSpawn = (Random.value > 0.8f) ? playerChaserPrefab : defenseChaserPrefab;
+        GameObject prefabToSpawn = (Random.value > 0.75f) ? playerChaserPrefab : defenseChaserPrefab;
 
         Instantiate(prefabToSpawn, chosenSpawn.position, Quaternion.identity);
     }
