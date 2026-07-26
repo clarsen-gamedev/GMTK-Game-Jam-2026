a93416058a6149fb2ad8d9d97a089ff6fb3f4a61
Connor Larsen <71350625+clarsen-gamedev@users.noreply.github.com>
2026-07-26 10:38:30 -0400

    title

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -142,7 +142,7 @@ public class AudioManager : MonoBehaviour
     // Notification Sounds
     public void PlaySpawnRateIncreaseSound() => PlayOneShot(spawnRateIncreaseClip, 0.3f);
     public void PlaySpawnRateDecreaseSound() => PlayOneShot(spawnRateDecreaseClip, 0.7f);
-    public void PlayTimerGainSound() => PlayOneShot(timerGainClip);
+    public void PlayTimerGainSound() => PlayOneShot(timerGainClip, 0.7f);
     public void PlayTimerLossSound() => PlayOneShot(timerLossClip);
     public void PlayDefenseSwitchSound() => PlayOneShot(defenseSwitchClip);
 
